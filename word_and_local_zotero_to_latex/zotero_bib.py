e5faed5128d451f626afe5e1724347643ee5aab5
jlandert <jan.landert@fibl.org>
2026-03-11 15:04:25 +0100

    Removed print command for clearer output.

--- word_and_local_zotero_to_latex/zotero_bib.py
+++ word_and_local_zotero_to_latex/zotero_bib.py
@@ -12,7 +12,7 @@ def get_bibtex_from_zotero(title: str) -> str:
     Query local Zotero and return a BibTeX block (as text) for a title string.
     """
     zotero_client = zotero.Zotero(
-        library_id="5640580", # https://groups.google.com/g/zotero-dev/c/ElvHhIFAXrY/m/fA7SKKwsAgAJ?pli=1
+        library_id="5639840", # https://groups.google.com/g/zotero-dev/c/ElvHhIFAXrY/m/fA7SKKwsAgAJ?pli=1
         library_type="group",
         api_key=None,
         local=True,
@@ -36,7 +36,6 @@ async def fetch_bibtex_for_titles(
 
     async def _one(title: str) -> tuple[str, str]:
         async with sem:
-            print("querying: ", title)
             bib = await asyncio.to_thread(get_bibtex_from_zotero, title)
             bib = bib.strip() if isinstance(bib, str) else str(bib).strip()
             return title, bib
